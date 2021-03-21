31dcee14ebcce188b51b9f3a360614d1e2a0eb59
Milena Verissimo <milenaverissimo@gmail.com>
2021-03-21 18:07:23 -0300

    Criando dao para ideia

---
+++ src/main/java/com/mycompany/plugue/dao/IdeiaDAO.java
@@ -0,0 +1,24 @@
+package com.mycompany.plugue.dao;
+import com.mycompany.plugue.persistencia.Ideia;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+/**
+ * @author Milena
+ */
+public class IdeiaDAO {
+    
+    private EntityManager em;
+    
+    public void newIdeia(Ideia ideia){}
+    
+    public void updateIdeia(Ideia ideia) {}
+    
+    public Ideia getIdea() {
+        return null;
+    }
+    
+    public List<Ideia> getIdeas(){
+        return null;
+    }
+}
